2867d42eb41e3b46a739d962a3f7d83ba874b64b
markmcgrory <mark_mcgrory@hotmail.co.uk>
2023-08-25 20:39:20 +0100

    models folder added + classes drafted

---
+++ models/book.py
@@ -0,0 +1,10 @@
+class Book:
+    
+    def __init__ (self, title, author, genre, publisher, copies, price, id = None)
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.publisher = publisher
+        self.copies = copies
+        self.price = price
+        self.id = id
---
+++ models/publisher.py
@@ -0,0 +1,7 @@
+class Publisher:
+    
+    def __init__ (self, name, email_address, website, id = None):
+        self.name = name
+        self.email_address = email_address
+        self.website = website
+        self.id = id
