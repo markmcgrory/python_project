14bddcde05560091649ccb98764bce97d052e714
markmcgrory <mark_mcgrory@hotmail.co.uk>
2023-08-26 12:14:35 +0100

    some more empty files added

--- models/book.py
+++ models/book.py
@@ -1,6 +1,6 @@
 class Book:
     
-    def __init__ (self, title, author, genre, publisher, copies, price, id = None)
+    def __init__ (self, title, author, genre, publisher, copies, price, id = None):
         self.title = title
         self.author = author
         self.genre = genre
